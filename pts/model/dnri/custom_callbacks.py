Improved link prediction; added option of undirected random graph; fixed normalization in message passing

pts/model/dnri/custom_callbacks.py
@@ -35,6 +35,7 @@ class ReplaceEdgeCallback(Callback):
         self.post_epochs = post_epochs
         self.mod_freq = mod_freq
         self.num_mods = num_mods
+
     
     def on_train_epoch_end(self, trainer, pl_module):
         epoch = pl_module.current_epoch
@@ -44,8 +45,6 @@ class ReplaceEdgeCallback(Callback):
             _, edge_idx_list = (-mean_list).topk(self.num_mods)
             
             target_dim = pl_module.model.target_dim
-            # edges = np.stack(torch.load(str(args.path)+'edges.pt')).T
-            # edges = np.stack(np.load(str(args.path)+'edges.npy')).T
             edges = np.stack(pl_module.model.edges, axis=1)
             num_edges = len(edges)
             
@@ -64,6 +63,60 @@ class ReplaceEdgeCallback(Callback):
                         
             edges = [edges[:,0].squeeze(), edges[:,1].squeeze()]
             torch.save(edges, str(args.path)+'edges.pt')
-            # np.save(str(args.path)+'edges', edges)
+            
+        return
+
+
+class ReplaceEdgeCallback_upd(Callback):
+    def __init__(
+        self,
+        num_edges: int,
+        pre_epochs: int = 5,
+        post_epochs: int = 5,
+        mod_freq: int = 1,
+        num_mods: int = 1,
+    ):
+        super().__init__()
+        
+        self.num_edges = num_edges
+        self.pre_epochs = pre_epochs
+        self.post_epochs = post_epochs
+        self.mod_freq = mod_freq
+        self.num_mods = num_mods
+
+    
+    def on_train_epoch_end(self, trainer, pl_module):
+        epoch = pl_module.current_epoch
+        if epoch >= (self.pre_epochs - 1) and (epoch + 1 - self.pre_epochs) % self.mod_freq == 0 and epoch < trainer.max_epochs - self.post_epochs - 1:
+            edge_usage_list = pl_module.edge_usage_list
+            mean_list = torch.cat(edge_usage_list, dim=0).mean(0) # mean accross all batches (dim=0)
+            _, indices = (-mean_list).topk(self.num_mods)
+
+            edges = np.stack(torch.load(str(args.path)+'edges.pt'), axis=1)
+
+            target_dim = pl_module.model.target_dim
+            count = (indices < self.num_edges).sum()
+            
+            print("Discarded edge(s): "+str(edges[indices.cpu(),:]))
+            print('Changed edges: %d/%d' % (count, self.num_mods))
+            
+            edges = np.delete(edges, indices.cpu(), axis=0)
+
+            combined_edges = edges
+            candidate_list = []
+            while len(candidate_list) < self.num_mods:
+                print("Sampling new edge candidate...")
+                # sample new edge
+                new_edge = np.random.choice(target_dim, 2)
+                if new_edge[0] != new_edge[1]:
+                    combined_edges, counts = np.unique(np.concatenate([combined_edges, new_edge.reshape(1,-1)], axis=0), axis=0, return_counts=True)
+                    if counts.max() == 1:
+                        print("New edge candidate: "+str(new_edge))
+                        candidate_list.append(new_edge.reshape(1,-1))
+
+            edges = np.concatenate([edges, np.concatenate(candidate_list, axis=0)], axis=0)
+       
+            edges = [edges[:,0].squeeze(), edges[:,1].squeeze()]
+            torch.save(edges, str(args.path)+'edges.pt')
             
         return

pts/model/dnri/dnri.py
@@ -9,7 +9,7 @@ from gluonts.dataset.multivariate_grouper import MultivariateGrouper
 from pts.model.dnri import DNRIEstimator
 from pts.modules import StudentTOutput
 
-from utils import construct_full_graph, construct_expander, construct_expander_fast, construct_random_graph, construct_bipartite_graph
+from utils import construct_full_graph, construct_expander, construct_expander_fast, construct_random_graph, construct_bipartite_graph, construct_random_directed_graph
 
 import wandb
 # wandb.init(project="dnri-link-prediction")
@@ -63,21 +63,23 @@ if args.graph_constr == "bipartite":
     print("Graph construction: Bipartite graph")
     edges = construct_bipartite_graph(target_dim, args.graph_density)
 
+# sparse random directed graph
+if args.graph_constr == "random-directed":
+    print("Graph construction: Random directed graph")
+    edges, num_edges_used = construct_random_directed_graph(target_dim, args.graph_density, args.num_mods)
+
 torch.save(edges, '/network/scratch/f/frederik.wenkel/dnri/edges.pt')
-# np.save(str(args.path)+'edges.npy', edges)
 
 dataset_train = train_grouper(dataset.train)
 dataset_test = test_grouper(dataset.test)
 
-from custom_callbacks import ReplaceEdgeCallback, EdgeUsageCallback
-from pytorch_lightning.callbacks import ModelCheckpoint
+from custom_callbacks import ReplaceEdgeCallback, ReplaceEdgeCallback_upd, EdgeUsageCallback
 
-# callback_list = [ModelCheckpoint(dirpath='./checkpoints', filename='{epoch}', save_top_k=-1, every_n_epochs=2)]
 callback_list = []
 
 if args.link_prediction == 1:
     print("Auxiliary link prediction...")
-    callback_list.append(ReplaceEdgeCallback(args.pre_epochs, args.post_epochs, args.mod_freq, args.num_mods))
+    callback_list.append(ReplaceEdgeCallback_upd(num_edges_used, args.pre_epochs, args.post_epochs, args.mod_freq, args.num_mods))
 
 estimator = DNRIEstimator(
     freq=dataset.metadata.freq,
@@ -92,13 +94,12 @@ estimator = DNRIEstimator(
     decoder_hidden=args.hidden_dim_dec,
     rnn_hidden_size=args.hidden_dim_rnn,
     edges=edges,
-    link_prediction = args.link_prediction,
+    num_edges_used=num_edges_used,
     num_layers=args.num_layers,
     
     # training hyperparams
     batch_size=args.batch_size,
     num_batches_per_epoch=args.num_batches_per_epoch,
-    # trainer_kwargs=dict(max_epochs=args.num_epochs, accelerator='gpu', gpus=1, callbacks=callback_list, logger=wandb_logger),
     trainer_kwargs=dict(max_epochs=args.num_epochs, accelerator='gpu', gpus=1, callbacks=callback_list),
 )
 

pts/model/dnri/estimator.py
@@ -52,12 +52,11 @@ class DNRIEstimator(PyTorchLightningEstimator):
     @validated()
     def __init__(
         self,
-        # trainer,
         freq: str,
         prediction_length: int,
         target_dim: int,  # Multivariate dimension of the input features
         edges: list,
-        link_prediction: bool = False,
+        num_edges_used: int,
         num_layers: int = 1,
         mlp_hidden_size: int = 64,
         decoder_hidden: int = 64,
@@ -125,7 +124,7 @@ class DNRIEstimator(PyTorchLightningEstimator):
 
         # for sparse message passing
         self.edges = edges
-        self.link_prediction = link_prediction
+        self.num_edges_used = num_edges_used
         self.num_layers = num_layers
 
         self.embedding_dimension = (
@@ -307,6 +306,7 @@ class DNRIEstimator(PyTorchLightningEstimator):
             target_dim=self.target_dim,
             rnn_hidden_size=self.rnn_hidden_size,
             edges=self.edges,
+            num_edges_used=self.num_edges_used,
             num_layers=self.num_layers,
             cell_type=self.cell_type,
             context_length=self.context_length,
@@ -338,7 +338,6 @@ class DNRIEstimator(PyTorchLightningEstimator):
             input_names=PREDICTION_INPUT_NAMES,
             prediction_net=module.model,
             batch_size=self.batch_size,
-            freq=self.freq,
             prediction_length=self.prediction_length,
             device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         )

pts/model/dnri/lightning_module.py
@@ -40,7 +40,7 @@ class DNRILightningModule(pl.LightningModule):
         self.lr = lr
         self.weight_decay = weight_decay
         
-        self.edge_usage = None
+        self.edge_usage = []
 
     def _compute_loss(self, batch):
         feat_static_cat = batch["feat_static_cat"]
@@ -53,7 +53,6 @@ class DNRILightningModule(pl.LightningModule):
         future_observed_values = batch["future_observed_values"]
         
         self.model.edges = torch.load(str(args.path)+'edges.pt')
-        # self.model.edges = np.load(str(args.path)+'edges.npy')
 
         # encoder
         (

pts/model/dnri/module.py
@@ -71,7 +71,9 @@ class DNRI_Encoder(nn.Module):
             self.mlp_edge_list.append(MLP(mlp_hidden_size * 2, [mlp_hidden_size, mlp_hidden_size]))
             self.mlp_node_list.append(MLP(mlp_hidden_size, [mlp_hidden_size, mlp_hidden_size]))
         
-        self.mlp_out = MLP(mlp_hidden_size * 3, [mlp_hidden_size, mlp_hidden_size])
+        # self.mlp_out = MLP(mlp_hidden_size * 3, [mlp_hidden_size, mlp_hidden_size])
+        # edit for skip connection
+        self.mlp_out = MLP(mlp_hidden_size * (num_layers+2), [mlp_hidden_size, mlp_hidden_size])
 
         if rnn_hidden_size is None:
             rnn_hidden_size = mlp_hidden_size
@@ -150,7 +152,12 @@ class DNRI_Encoder(nn.Module):
             x = self.mlp_node_list[k](x)  # [B, D, T, F]
         
         x = self.node2edge(x)  # [B, D^2, T, F*2]
-        x = torch.cat((x, x_skip_list[0]), dim=-1)  # [B, D^2, T, F*3]
+
+        # x = torch.cat((x, x_skip_list[0]), dim=-1)  # [B, D^2, T, F*3]
+        # edit for skip connection
+        x_skip_list.append(x)
+        x = torch.cat(x_skip_list, dim=-1)  # [B, D^2, T, F*(num_layers+2))]
+
         
         x = self.mlp_out(x)  # [B, D^2, T, F]
 
@@ -191,13 +198,14 @@ class DNRI_Decoder(nn.Module):
         dropout_rate,
         distr_output,
         gumbel_temp,
-        edges,
+        num_edges_used,
         num_layers,
         num_edge_types=2,
     ):
         super().__init__()
 
         self.target_dim = target_dim
+        self.num_edges_used = num_edges_used
         self.num_layers = num_layers
         self.num_edge_types = num_edge_types
         self.msg_out_shape = decoder_hidden
@@ -237,13 +245,6 @@ class DNRI_Decoder(nn.Module):
             decoder_hidden * self.target_dim
         )
 
-        self.send_edges = edges[0]
-        self.recv_edges = edges[1]
-        
-        self.register_buffer(
-            "edge2node_mat", torch.Tensor(encode_onehot_mod(self.recv_edges, target_dim))
-        )
-        
         # # for sparse message passing
         # self.register_buffer(
         #     "edge2node_mat", encode_onehot_sparse(self.recv_edges, target_dim)
@@ -258,29 +259,27 @@ class DNRI_Decoder(nn.Module):
         
         # load modified edges
         edges = torch.load(str(args.path)+'edges.pt')
-        # edges = np.load(str(args.path)+'edges.npy')
-        self.send_edges = edges[0]
-        self.recv_edges = edges[1]
+        self.send_edges = edges[0][:self.num_edges_used]
+        self.recv_edges = edges[1][:self.num_edges_used]
         
         old_shape = edge_logits.shape
         edges = F.gumbel_softmax(
             edge_logits.reshape(-1, self.num_edge_types),
             tau=self.gumbel_temp,
             hard=hard_sample,
-        ).view(old_shape)
+        ).view(old_shape) # [B, num_edges, edges_types]
         
         old_shape = hidden.shape
-        hidden_ = hidden
+        hidden_ = hidden # [B, target_dim, msg_out (F)]
         
         # for skip connection
         agg_msgs_list = []
-        
         for k in range(self.num_layers):
             # node2edge
             receivers = hidden_[:, self.recv_edges, ...]
             senders = hidden_[:, self.send_edges, ...]
 
-            # pre_msg: [batch, num_edges, 2*msg_out]
+            # pre_msg: [batch, num_edges_used, 2*msg_out]
             pre_msg = torch.cat([receivers, senders], dim=-1)
 
             all_msgs = torch.zeros(
@@ -297,11 +296,12 @@ class DNRI_Decoder(nn.Module):
             # Run separate MLP for every edge type
             # NOTE: to exclude one edge type, simply offset range by 1
             for i in range(start_idx, len(self.msg_fc2_list[k])):
-                msg = torch.tanh(self.msg_fc1_list[k][i](pre_msg))
+                msg = torch.tanh(self.msg_fc1_list[k][i](pre_msg)) # [B, num_edges_used, F]
                 if self.training:
                     msg = F.dropout(msg, p=self.dropout_rate)
                 msg = torch.tanh(self.msg_fc2_list[k][i](msg))
-                msg = msg * edges[:, :, i : i + 1]
+                msg = msg * edges[:, :self.num_edges_used, i : i + 1]
+                # msg = msg * edges[:, :, i : i + 1]
                 all_msgs += msg / norm
 
             # dense message passing
@@ -364,6 +364,7 @@ class DNRIModel(nn.Module):
         gumbel_temp,
         rnn_hidden_size,
         edges: list,    # for sparse message passing
+        num_edges_used: int, # number us "used" edges
         num_layers: int = 1,
         embedding_dimension: Optional[List[int]] = None,
         lags_seq: Optional[List[int]] = None,
@@ -375,6 +376,7 @@ class DNRIModel(nn.Module):
         super().__init__()
         
         self.edges = edges
+        self.num_edges_used = num_edges_used
 
         self.target_dim = target_dim
         self.distr_output = distr_output
@@ -417,7 +419,7 @@ class DNRIModel(nn.Module):
             distr_output=distr_output,
             num_edge_types=num_edge_types,
             gumbel_temp=gumbel_temp,
-            edges=self.edges,
+            num_edges_used=self.num_edges_used,
             num_layers=num_layers,
         )
 

pts/model/dnri/utils.py
@@ -84,7 +84,7 @@ def construct_expander_fast(target_dim, density):
     print("Desired density: "+str(density))
     print("Actual density: "+str(np.round(len(edges)/2/num_edges_full, 3)))
     
-    return edges[0,:].squeeze(), edges[1,:].squeeze()
+    return [edges[0,:].squeeze(), edges[1,:].squeeze()]
 
 
 def construct_random_graph(target_dim, density):
@@ -106,7 +106,7 @@ def construct_random_graph(target_dim, density):
     print("Actual density: "+str(np.round(len(send_edges)/num_edges_full, 3)))
     
     # make edges bi-directional
-    return np.concatenate((send_edges, recv_edges), axis=0), np.concatenate((recv_edges, send_edges), axis=0)
+    return [np.concatenate((send_edges, recv_edges), axis=0), np.concatenate((recv_edges, send_edges), axis=0)]
 
 
 def construct_bipartite_graph(target_dim, density):
@@ -137,4 +137,50 @@ def construct_bipartite_graph(target_dim, density):
     print("Desired density: "+str(density))
     print("Actual density: "+str(np.round(len(send_edges)/2/num_edges_full, 3)))
     
-    return np.array(send_edges), np.array(recv_edges)
+    return [np.array(send_edges), np.array(recv_edges)]
+
+
+# def construct_random_directed_graph(target_dim, density):
+#     num_edges_full = (target_dim ** 2 - target_dim)
+#     num_edges_target = int(np.floor(density * num_edges_full))
+    
+#     all_edges = np.where(np.ones(target_dim) - np.eye(target_dim))
+    
+#     all_send_edges = all_edges[0]
+#     all_recv_edges = all_edges[1]
+    
+#     reorder = np.random.permutation(len(all_send_edges))
+    
+#     send_edges = all_send_edges[reorder][:num_edges_target]
+#     recv_edges = all_recv_edges[reorder][:num_edges_target]
+
+#     num_edges = int(len(send_edges))
+    
+#     print("Number of directed edges: "+str(num_edges))
+#     print("Desired density: "+str(density))
+#     print("Actual density: "+str(np.round(len(send_edges)/num_edges_full, 3)))
+    
+#     return [send_edges, recv_edges], num_edges
+
+
+def construct_random_directed_graph(target_dim, density, num_mods):
+    num_edges_full = (target_dim ** 2 - target_dim)
+    num_edges_target = int(np.floor(density * num_edges_full)) + num_mods
+    
+    all_edges = np.where(np.ones(target_dim) - np.eye(target_dim))
+    
+    all_send_edges = all_edges[0]
+    all_recv_edges = all_edges[1]
+    
+    reorder = np.random.permutation(len(all_send_edges))
+    
+    send_edges = all_send_edges[reorder][:num_edges_target]
+    recv_edges = all_recv_edges[reorder][:num_edges_target]
+
+    num_edges_used = int(len(send_edges)) - num_mods
+    
+    print("Number of directed edges: "+str(num_edges_used))
+    print("Desired density: "+str(density))
+    print("Actual density: "+str(np.round(len(send_edges)/num_edges_full, 3)))
+    
+    return [send_edges, recv_edges], num_edges_used